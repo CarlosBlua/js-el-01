be938337893476090238c9e24efbb086d18ad116
CarlosBlua <carlos.blua@gmail.com>
2022-05-04 18:46:53 -0300

    nueva version 2022-05-04

---
+++ js/csb-ej.js
@@ -0,0 +1,21 @@
+// var CostoProducto = (115)
+// console.log("Hola mundo es mi primer script");
+// console.log(238467923)
+// console.log("238742")
+// console.log(CostoProducto)
+
+// let num1 = 2;
+// let num2 = 3;
+
+// resultado = num1 + num2;
+// console.log(resultado);
+
+// let edadJuan = 28, edadMirta = 15, edadCarlos = 20;
+
+// if(edadJuan >= edadCarlos){
+//     console.log("Juan es mayor que Carlos");
+// }
+// else{
+//     console.log("Carlos es Mayor que Juan");
+// }
+document.getElementById("demo").innerHTML = "Hola, esto es algo distinto"
